83e03b4694ea215e5c2a5b032a679d666da79fa5
Vishal Solanki <vcsolanki.vs@gmail.com>
2022-12-01 13:49:55 +0530

    Added Composition Example

--- ObjectOrientedDesign/Composition.java
+++ ObjectOrientedDesign/Composition.java
@@ -1,5 +1,15 @@
 package ObjectOrientedDesign;
 
+import ObjectOrientedDesign.Components.Vehicle;
+
+/*
+ * Vehicle object cannot exist without Wheel object.
+ */
+
 public class Composition {
-    
+    public static void main(String[] args) {
+        var vh = new Vehicle();
+        vh.setWheel(4, 35.56f);
+        System.out.println("Vehicle with "+vh.getWheelCount()+" wheels!");
+    }
 }
